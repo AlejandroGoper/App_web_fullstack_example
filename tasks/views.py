4e4baf41bb9166e3ff313d4c65b2d9a57c02bde9
AlejandroGoper <alejandrogoper.11@gmail.com>
2025-04-16 12:04:36 -0600

    Configuration of views

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import task_serializer
+from .models import task
 
 # Create your views here.
+class TaskView(viewsets.ModelViewSet):
+    serializer_class = task_serializer
+    queryset = task.objects.all()
