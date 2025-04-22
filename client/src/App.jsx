abbdc2abf65bf9fb18dbb969335605641d987146
AlejandroGoper <alejandrogoper.11@gmail.com>
2025-04-22 16:50:23 -0600

    Adding another route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom"
 import { TasksPage } from "./pages/TasksPage"
-
+import { TaskFormPage } from "./pages/TaskFormPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/tasks" element={< TasksPage />} />
+        <Route path="/tasks-create" element={< TaskFormPage />} />
       </Routes>
     </BrowserRouter>
   );
