f63f64a212986832472dc06e658f444cdc407691
AlejandroGoper <alejandrogoper.11@gmail.com>
2025-04-22 19:10:07 -0600

    GET method implemented, displayin in console

---
+++ client/src/components/TasksList.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react"
+import { getAllTasks } from "../api/tasks.api";
+
+export function TasksList() {
+    useEffect(() => {
+        async function loadTasks(){
+            const res = await getAllTasks();
+            console.log(res);
+        }
+        loadTasks();    
+    },[]);
+    return <div>TasksList</div>
+}
